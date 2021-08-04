c0423676cfb9e243f6b6c2c32a24f6301220aacd
Larissa Pereira <larissa.pereira382@gmail.com>
2021-08-04 15:15:17 -0300

    teste de variáveis

---
+++ sintaxe-variaveis-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,15 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+		int idade;
+		idade = 30;
+		System.out.println(idade);
+		idade = 25 + 5;
+		System.out.println(idade);
+		idade = 5*5+5;
+		System.out.println(idade);
+	}
+
+}
+
