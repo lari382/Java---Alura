4a002728928c8a657acd1da957dd1535d1ff9fc4
Larissa Pereira <67720931+lari382@users.noreply.github.com>
2021-08-09 17:28:34 -0300

    comit final aula 5

---
+++ sintaxe-variaveis-fluxo/src/TestaChar.java
@@ -0,0 +1,19 @@
+
+public class TestaChar {
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		char valor = 66;
+		System.out.println(valor);
+		valor = (char)(valor+1);
+		System.out.println(valor);
+		
+		String palavra = "Os cursos da Alura são excelentes";
+		System.out.println(palavra);
+		
+		palavra = palavra + " mas fica meio caro";
+		System.out.println(palavra);
+
+	}
+
+}
