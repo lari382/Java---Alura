539b4d4bed16da1dc8806e59ea716692481625df
Larissa Pereira <lapereira@ibm.com>
2021-08-09 14:27:20 -0300

    comit aula 4

---
+++ sintaxe-variaveis-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,13 @@
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario;
+		salario = 3100.00;
+		System.out.println("meu salário é " + salario);
+		double divisao;
+		divisao = 5.0/2;
+		System.out.println(divisao);
+		double outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+	}
+
+}
