88a99d713862094ba89599af54b7563aec58fa00
Larissa Pereira <67720931+lari382@users.noreply.github.com>
2021-08-09 18:03:12 -0300

    Create TestaCondicional.java

---
+++ Java JRE e JDK Escreva o seu primeiro código com Eclipse/sintaxe-variaveis-fluxo/src/TestaCondicional.java
@@ -0,0 +1,17 @@
+
+public class TestaCondicional {
+
+	public static void main(String[] args) {
+		int idade = 17;
+		int quantidadePessoas = 3;
+		if (idade >= 18) {
+			System.out.println("você é maior de idade");
+		} else if (quantidadePessoas >= 2) {
+			System.out.println("voce e menor mas esta acompanhado");
+		} else {
+			System.out.println("voce e menor de idade");
+		}
+
+	}
+
+}
