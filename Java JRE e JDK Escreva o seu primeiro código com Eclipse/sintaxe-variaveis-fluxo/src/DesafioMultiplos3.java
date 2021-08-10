a7996ae15fa33fbd24710c10f6ac94a41d43d49b
Larissa Pereira <67720931+lari382@users.noreply.github.com>
2021-08-10 15:53:10 -0300

    Create DesafioMultiplos3.java

---
+++ Java JRE e JDK Escreva o seu primeiro código com Eclipse/sintaxe-variaveis-fluxo/src/DesafioMultiplos3.java
@@ -0,0 +1,11 @@
+
+public class DesafioMultiplos3 {
+	public static void main(String[] args) {
+		for(int multiplos = 1; multiplos <= 100; multiplos++) {
+			if(multiplos % 3 == 0) {
+				System.out.println(multiplos);
+			}
+		}
+	}
+
+}
