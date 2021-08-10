52deecc11cfe19c9b03b0f214843242647bfb0c0
Larissa Pereira <67720931+lari382@users.noreply.github.com>
2021-08-10 14:46:18 -0300

    Create TestaLaco.java

---
+++ Java JRE e JDK Escreva o seu primeiro código com Eclipse/sintaxe-variaveis-fluxo/src/TestaLaco.java
@@ -0,0 +1,12 @@
+
+public class TestaLaco {
+	public static void main(String[] args) {
+		int contador = 0;
+		while (contador <= 9) {
+			//contador = contador +1;
+			//contador += 1;
+			contador ++;
+			System.out.println(contador);
+		}
+	}
+}
