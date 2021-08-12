436c47757b197b0281d0cd02e9c68086b1b8d56d
Larissa Pereira <67720931+lari382@users.noreply.github.com>
2021-08-12 09:03:41 -0300

    exercicio conta corrente

---
+++ java-introducao-orientacao-objetos/java-introducao-orientacao-objetos/src/ContaCorrente.java
@@ -0,0 +1,12 @@
+
+public class ContaCorrente {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println(segundaConta.saldo);
+	}
+}
