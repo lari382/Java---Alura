4ece5eef1a4c1ef582128bee44f8ea32b4bd5079
Larissa Pereira <larissa.pereira382@gmail.com>
2021-08-23 15:34:16 -0300

    aula 3

--- java-introducao-orientacao-objetos/java-introducao-orientacao-objetos/src/TestaMetodo.java
+++ java-introducao-orientacao-objetos/java-introducao-orientacao-objetos/src/TestaMetodo.java
@@ -4,9 +4,19 @@ public class TestaMetodo {
 		Conta contaDaLarissa = new Conta();
 		contaDaLarissa.deposita(150);
 		System.out.println(contaDaLarissa.saldo);
-		
+
 		boolean conseguiuSacar = contaDaLarissa.saca(250);
 		System.out.println(contaDaLarissa.saldo);
 		System.out.println(conseguiuSacar);
+
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.deposita(2000);
+		if (contaDoPaulo.transfere(350, contaDaLarissa)) {
+			System.out.println("transferencia efetuada com sucesso");
+		} else {
+			System.out.println("transferencia nao efetuada");
+		}
+		System.out.println("Saldo da conta de Larissa é " + contaDaLarissa.saldo);
+		System.out.println("Saldo da conta de Paulo é " + contaDoPaulo.saldo);
 	}
 }
