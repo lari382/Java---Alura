c4c33f107582e6b432f7e09ad7ece67334097527
Larissa Pereira <larissa.pereira382@gmail.com>
2021-08-23 17:32:28 -0300

    aula 4

---
+++ java-introducao-orientacao-objetos/bytebank-composto/src/TestaBanco.java
@@ -0,0 +1,14 @@
+
+public class TestaBanco {
+	public static void main(String[] args) {
+		Cliente larissa = new Cliente();
+		larissa.nome = "Larissa Pereira";
+		larissa.cpf = "123.456.789-10";
+		larissa.profissao = "Programadora";
+
+		Conta contaDaLarissa = new Conta();
+		contaDaLarissa.titular = larissa;
+		System.out.println(contaDaLarissa.titular.profissao);
+
+	}
+}
